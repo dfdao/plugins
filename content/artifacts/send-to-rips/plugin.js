52e1a9b378844a15100f75a4643b76b9dee9c2ec
tony-goss <anthony.goss@tufts.edu>
2021-06-05 16:06:46 -0700

    send artifacts to rips outline

---
+++ content/artifacts/send-to-rips/plugin.js
@@ -0,0 +1,29 @@
+/**
+ * Plugin:  
+ * Description: 
+ * Author(s): 
+ * 
+ * 
+ * Features:
+ 
+ * Todo:
+ */
+
+class Plugin {
+  constructor() { }
+
+  /**
+   * Called when plugin is launched with the "run" button.
+   */
+  async render(container) { }
+
+  /**
+   * Called when plugin modal is closed.
+   */
+  destroy() { }
+}
+
+/**
+ * And don't forget to register it!
+ */
+export default Plugin;
