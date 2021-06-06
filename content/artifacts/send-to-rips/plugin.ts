82a9cd6df99fbbbce5edfa774052d56f33c670e3
tony-goss <anthony.goss@tufts.edu>
2021-06-05 22:58:25 -0700

    update comments

---
+++ content/artifacts/send-to-rips/plugin.ts
@@ -0,0 +1,31 @@
+/**
+ * Plugin:  
+ * Description: 
+ * Author(s): 
+ * 
+ * 
+ * Features:
+ * Send all epic artifact to a level 6 spacetime rip
+ * 
+ 
+ * Todo:
+ */
+
+class Plugin {
+  constructor() { }
+
+  /**
+   * Called when plugin is launched with the "run" button.
+   */
+  async render(container) { }
+
+  /**
+   * Called when plugin modal is closed.
+   */
+  destroy() { }
+}
+
+/**
+ * And don't forget to register it!
+ */
+export default Plugin;
