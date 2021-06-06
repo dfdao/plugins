c00cc1312152449da299f77568e6e60bd7ea6b67
tony-goss <anthony.goss@tufts.edu>
2021-06-06 13:58:39 -0700

    small plugins

--- content/utilities/count-artifacts/plugin.js
+++ content/utilities/count-artifacts/plugin.js
@@ -1,19 +1,19 @@
 /**
- * Plugin:  
- * Description: 
+ * Plugin:
+ * Description:
  *  Not working right now
- * 
+ *
  * Features:
- * 
+ *
 
- 
+
  * Todo:
  */
 
 import {
-  canHaveArtifact,
-  hasArtifact,
-  countPlanetArtifactRarity,
+    canHaveArtifact,
+    hasArtifact,
+    countPlanetArtifactRarity,
 } from 'https://dfdao.github.io/utils/artifacts.js';
 
 import * as ENUMS from 'https://dfdao.github.io/utils/enums.js';
@@ -21,35 +21,35 @@ import * as ENUMS from 'https://dfdao.github.io/utils/enums.js';
 console.log('enums', ENUMS)
 
 class Plugin {
-  constructor() { }
+    constructor() { }
 
-  /**
-   * Called when plugin is launched with the "run" button.
-   */
-  async render(container) {
-    container.style.width = '200px';
+    /**
+     * Called when plugin is launched with the "run" button.
+     */
+    async render(container) {
+        container.style.width = '200px';
 
-    let pilotButton = document.createElement('button');
-    pilotButton.innerHTML = `${'count artifacts'}`;
+        let pilotButton = document.createElement('button');
+        pilotButton.innerHTML = `${'count artifacts'}`;
 
 
-    pilotButton.onclick = () => {
+        pilotButton.onclick = () => {
 
-      const epic = ArtifactRarity.Epic
-      const mythic = ArtifactRarity.Mythic
-      const legendary = ArtifactRarity.Legendary
-      const count = countArtifacts(mythic)
-      pilotButton.innerHTML = `found ${count} ${ArtifactRarity[legendary]} artifacts`;
+            const epic = ArtifactRarity.Epic
+            const mythic = ArtifactRarity.Mythic
+            const legendary = ArtifactRarity.Legendary
+            const count = countArtifacts(mythic)
+            pilotButton.innerHTML = `found ${count} ${ArtifactRarity[legendary]} artifacts`;
+        }
+
+        container.appendChild(pilotButton);
     }
 
-    container.appendChild(pilotButton); 
-  }
-
-  /**
-   * Called when plugin modal is closed.
-   */
-  destroy() { 
-  }
+    /**
+     * Called when plugin modal is closed.
+     */
+    destroy() {
+    }
 
 }
 
